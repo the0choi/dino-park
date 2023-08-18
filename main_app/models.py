2647470cb659a67776e032f3fd70d827cc8b6fa7
the0choi <theodore.choi07@gmail.com>
2023-08-18 14:48:50 +1000

    delete sqlite3 file

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Field(models.Model):
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.date} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('fields_detail', kwargs={'field_id': self.id})
+
+class Dino(models.Model):
+    name = models.CharField(max_length=20)
+    duration = models.CharField(max_length=20)
+    url = models.URLField(max_length=200)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.duration} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('dinos_detail', kwargs={'dino_id': self.id})
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Field, Dino
+# from .forms import
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+@login_required
+def fields_index(request):
+    fields = Field.objects.filter(user=request.user)
+    return render(request, 'fields/index.html', {'fields': fields})
+
+
+@login_required
+def fields_detail(request, field_id):
+    field = Field.objects.get(id=field_id)
+    dinos = field.dinos.all().values_list('id')
+    return render(request, 'fields/detail.html', {'field': field, 'dinos': dinos})
+
+@login_required
+def dinos_detail(request, dino_id):
+    dino = Dino.objects.get(id=dino_id)
+    return render(request, 'dinos/detail.html', {'dino': dino})
+
