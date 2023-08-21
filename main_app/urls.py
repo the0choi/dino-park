6eef71cd89ea8e40cf810666f2572c73679cb215
the0choi <theodore.choi07@gmail.com>
2023-08-21 10:41:46 +1000

    add create field form

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,6 +6,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('fields/', views.fields_index, name='fields_index'),
     path('fields/<int:field_id>/', views.fields_detail, name='fields_detail'),
+    path('fields/create/', views.FieldCreate.as_view(), name='fields_create'),
+    path('fields/<int:pk>/delete/', views.FieldDelete.as_view(), name='fields_delete'),
     path('dinos/<int:dino_id>/', views.dinos_detail, name='dinos_detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
 ]
