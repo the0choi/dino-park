a63dc41e3fe7968da33650daf6b164002b400146
the0choi <theodore.choi07@gmail.com>
2023-08-29 15:59:02 +1000

    heroku deploy fixes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python3 manage.py migrate
-web: gunicorn project3.wsgi --log-file -
+web: gunicorn project3.wsgi --log-file - --preload
--- project3/settings.py
+++ project3/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 import environ
 import os
-# import django_heroku
-# import dj_database_url
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
